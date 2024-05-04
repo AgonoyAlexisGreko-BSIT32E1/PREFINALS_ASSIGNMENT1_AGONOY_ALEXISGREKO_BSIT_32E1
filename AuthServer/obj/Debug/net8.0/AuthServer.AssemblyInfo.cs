0d5b597773a47c284bdbea9ec8bc84336c4d1a5a
Engbino_KarlJeffer_BSIT422E1 <148577459+AgonoyAlexisGreko-BSIT32E1@users.noreply.github.com>
2024-05-04 09:14:20 +0800

    Auth service

--- AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae4618ed949218d294b8a82172ab787064d0dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a051e3e40f61905fe9eaf3bcf0387e37a6a62bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
